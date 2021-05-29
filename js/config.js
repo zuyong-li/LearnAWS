22dec1ffa873c7af71ac7e44439699a9e848f5cc
zuyong-li <zuyong-li@berkeley.edu>
2021-05-28 23:38:23 -0700

    updated config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-1_Hl1kkzG6u', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'thmngfm95ld3busg7766s270q', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
